d1cda95835e689f0937ca86225f10a0c394e28d5
acerion <acerion@wp.pl>
2020-10-24 21:14:56 +0200

    Improvements to clang-tidy script

--- src/libcw/clang_tidy.sh
+++ src/libcw/clang_tidy.sh
@@ -1,3 +1,9 @@
-clang-tidy -checks="*,-llvm-header-guard,-readability-braces-around-statements,-readability-else-after-return,-clang-diagnostic-deprecated-declarations" *.c *.h -- -I.. -I../cwutils/
+CHECK_INCLUDE_PATHS="-I. -I.. -I../cwutils/"
+
+CHECK_CHECKS="*"
+CHECK_CHECKS+=",-llvm-header-guard"
+CHECK_CHECKS+=",-readability-braces-around-statements,-readability-else-after-return,-clang-diagnostic-deprecated-declarations"
+
+clang-tidy -checks=$CHECK_CHECKS *.c *.h -- $CHECK_INCLUDE_PATHS
 # clang-tidy-10 -checks="*,-llvm-header-guard,-readability-braces-around-statements,-readability-else-after-return,-clang-diagnostic-deprecated-declarations,-hicpp-braces-around-statements,-readability-magic-numbers,-cppcoreguidelines-avoid-magic-numbers,-readability-redundant-control-flow" *.c *.h -- -I.. -I../cwutils/
 # clang-tidy-10 -checks="*,-llvm-header-guard,-readability-braces-around-statements,-readability-else-after-return,-clang-diagnostic-deprecated-declarations,-hicpp-braces-around-statements,-readability-magic-numbers,-cppcoreguidelines-avoid-magic-numbers,-readability-redundant-control-flow,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling" *.c *.h -- -I.. -I../cwutils/
